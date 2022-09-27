8d0dae84d00a23a1bd2ed255be5000489b2d5ab7
«Sergey» <fomin-sm1998@mail.ru>
2022-09-27 21:00:49 +0300

    ДЗ семинара №1

--- 3_Home_Work/1_Seminar/project_1.py
+++ 3_Home_Work/1_Seminar/project_1.py
@@ -5,7 +5,6 @@
 - 7 -> да
 - 1 -> нет'''
 
-
 '''number = int(input('Введите число: '))
 week_day = [1, 2, 3, 4, 5]
 week_ends = [6, 7]
@@ -16,12 +15,56 @@ elif number in week_ends:
     print(f'День {number} ЯВЛЯЕТСЯ выходным ')'''
 
 
-
-
 '''2.Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) =
 ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.'''
 
+'''X_ = True
+Y_ = True
+Z_ = True 
+if not(X_ or Y_ or Z_) == (not X_ and not Y_ and not Z_):
+    print(f'№1 Если X = {X_} Y = {Y_} Z = {Z_}, то утверждение True')
 
+X_ = False
+Y_ = True
+Z_ = True 
+if not(X_ or Y_ or Z_) == (not X_ and not Y_ and not Z_):
+    print(f'№2 Если X = {X_} Y = {Y_} Z = {Z_}, то утверждение True')
+
+X_ = False
+Y_ = False
+Z_ = True 
+if not(X_ or Y_ or Z_) == (not X_ and not Y_ and not Z_):
+    print(f'№3 Если X = {X_} Y = {Y_} Z = {Z_}, то утверждение True')
+
+X_ = False
+Y_ = True
+Z_ = False 
+if not(X_ or Y_ or Z_) == (not X_ and not Y_ and not Z_):
+    print(f'№4 Если X = {X_} Y = {Y_} Z = {Z_}, то утверждение True')
+
+X_ = False
+Y_ = False
+Z_ = False
+if not(X_ or Y_ or Z_) == (not X_ and not Y_ and not Z_):
+    print(f'№5 Если X = {X_} Y = {Y_} Z = {Z_}, то утверждение True')
+
+X_ = True
+Y_ = False
+Z_ = True
+if not(X_ or Y_ or Z_) == (not X_ and not Y_ and not Z_):
+    print(f'№6 Если X = {X_} Y = {Y_} Z = {Z_}, то утверждение True')
+
+X_ = True
+Y_ = False
+Z_ = False
+if not(X_ or Y_ or Z_) == (not X_ and not Y_ and not Z_):
+    print(f'№7 Если X = {X_} Y = {Y_} Z = {Z_}, то утверждение True')
+
+X_ = True
+Y_ = True
+Z_ = False
+if not(X_ or Y_ or Z_) == (not X_ and not Y_ and not Z_):
+    print(f'№8 Если X = {X_} Y = {Y_} Z = {Z_}, то утверждение True')'''
 
 
 '''3.Напишите программу, которая принимает на вход координаты точки (X и Y),
@@ -45,9 +88,6 @@ elif coordinate_x > 0 and coordinate_y < 0:
     print(f'Заданная точканаходится в 4 четверти ')'''
 
 
-
-
-
 '''4.Напишите программу, которая по заданному номеру четверти, показывает диапазон
 возможных координат точек в этой четверти (x и y).'''
 
@@ -61,9 +101,6 @@ elif result == 3:
     print(f'В заданной четверти значения х < 0 и у < 0')
 elif result == 4:
     print(f'В заданной четверти значения х > 0 и у < 0')'''
-
-
-
 
 
 '''5.Напишите программу, которая принимает на вход координаты двух точек и находит
